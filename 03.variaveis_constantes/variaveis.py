aad5e829a9ad9d20313fa543318991f6cdf6aff0
stfnrosa <rosaa.estefanii@gmail.com>
2024-03-19 15:29:48 -0300

    renomeando

---
+++ 03.variaveis_constantes/variaveis.py
@@ -0,0 +1,14 @@
+age = 23
+name = 'Estefani'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+name,age = 'Janaina', 25
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+BRAZILIAN_STATES = ['SP','RJ','MG','SC','RS']
+print(BRAZILIAN_STATES)
+
+BRAZILIAN_STATES = 26
+print(BRAZILIAN_STATES)
+
+limite_saque_diario = 1000
